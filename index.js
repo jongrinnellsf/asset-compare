92646a19b4fc7dfa74804f988b6dc1472a3ff4a8
Jon Grinnell <jgrinnell0@gmail.com>
2019-08-25 14:01:47 -0700

    minor updates to styles, JS cleanup

--- index.js
+++ index.js
@@ -5,34 +5,37 @@ const cryptoURL = 'https://data.messari.io/api/v1/assets/';
 const stockURL = 'https://cloud.iexapis.com/v1/stock/';
 
 function formatQueryParams(params) {
-  const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-  return queryItems.join('&');
+    const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+    return queryItems.join('&');
 }
 
 function displayStockResults(responseJson) {
-    let marketCap = responseJson.marketCap.toFixed(2)
+    const marketCap = responseJson.marketCap.toFixed(2)
+    const symbol = responseJson.symbol;
+    const price = responseJson.latestPrice;
+    const name = responseJson.companyName;
         $('#results-list').append(
-        `<li id= "${responseJson.symbol}"><img src="assets/c2.png">
-        <p>${responseJson.companyName} (${responseJson.symbol})</p>
-        <p class="price">Current price: $${responseJson.latestPrice}</p>
+        `<li id= "${symbol}"><img src="assets/c2.png" alt="image of line chart icon for stocks">
+        <p>${name} (${symbol})</p>
+        <p class="price">Current price: $${price}</p>
         <p>Market cap: <span id="${marketCap}"> $${formatToUnits(marketCap, 2)}</span></p>
-        <button id="deleteAsset"><i class="fas fa-times"></i></button>
+        <button id="deleteAsset" class="delete"><i class="fas fa-times"></i></button>
         </li>`);
     $('#asset-list').removeClass('hidden');
-    addData(responseJson.symbol, marketCap, 'rgb(33, 206, 153, 0.4)');
+    addData(symbol, marketCap, 'rgb(33, 206, 153, 0.4)');
 }
 
 function displayCryptoResults(responseJson) {
     const marketCap = responseJson.data.marketcap.current_marketcap_usd.toFixed(2);
     const symbol = responseJson.data.symbol;
     const price = responseJson.data.market_data.price_usd.toFixed(2);
+    const name = responseJson.data.name
         $('#results-list').append(
-        `<li id = "${symbol}"><img src="assets/s2.png">
-        <p>${responseJson.data.name} (${symbol})</p>
+        `<li id = "${symbol}"><img src="assets/s2.png" alt="image of line chart icon for cryptoassets">
+        <p>${name} (${symbol})</p>
         <p class="price">Current price: $${price}</p>
         <p>Market cap: <span id="${marketCap}"> $${formatToUnits(marketCap, 2)}</span></p>
-        <button id="deleteAsset"><i class="fas fa-times"></i></button>
+        <button id="deleteAsset" class="delete"><i class="fas fa-times"></i></button>
         </li>`);
     $('#asset-list').removeClass('hidden');
     addData(symbol, marketCap, 'rgb(75, 0, 130, 0.4)'); 
@@ -45,26 +48,26 @@ function getStockData(query) {
     const queryString = formatQueryParams(params)
     const url = stockURL + query + '/quote?' + queryString
     fetch(url)
-      .then(response => {
+    .then(response => {
         if (response.ok) {
-          return response.json();
+            return response.json();
         }
         throw new Error(response.statusText);
-      })
-      .then(responseJson => displayStockResults(responseJson))
-      .catch(err => {
-        $('#js-error-message').text(`Ah sorry, we could not find that asset. Please try again. `);
-      });
+    })
+    .then(responseJson => displayStockResults(responseJson))
+    .catch(err => {
+    $('#js-error-message').text(`Ah sorry, we could not find that asset. Please try again.`);
+    });
 }
 
 function getCryptoData(query) {
     const url = cryptoURL + query + '/metrics'
     fetch(url)
     .then(response => {
-    if (response.ok) {
-        return response.json();
-    }
-    throw new Error(response.statusText);
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
     })
     .then(responseJson => displayCryptoResults(responseJson))
     .catch(err => {
@@ -115,15 +118,16 @@ const myChart = new Chart(ctx, {
     }
 });
 
-
+//format market cap #s (millions, billions, trillions, etc.)
 function formatToUnits(number, precision) {
     const abbrev = ['', 'K', 'M', 'B', 'T'];
     const unrangifiedOrder = Math.floor(Math.log10(Math.abs(number)) / 3)
-    const order = Math.max(0, Math.min(unrangifiedOrder, abbrev.length -1))
+    const order = Math.max(0, Math.min(unrangifiedOrder, abbrev.length - 1))
     const suffix = abbrev[order];
     return (number / Math.pow(10, order * 3)).toFixed(precision) + suffix;
 }
 
+//return chart to center if all li's are deleted
 function resetCenter() {
     const list = $('#results-list li');
     if (list.length === 0) {
@@ -145,6 +149,7 @@ function handleDeleteItemClicked() {
     });
 }
 
+//find the indexes of label, number and color stored in chart data, remove them, and delete the li
 function deleteItemClicked() {
     const id = $(event.target).closest('li').attr('id');
     const num = $(event.target).closest('li').find('p > span').attr('id');
@@ -158,7 +163,7 @@ function deleteItemClicked() {
 }
 
 function noSpaces() {
-    $('form').on('keypress', function(event) {
+    $('#asset-form').on('keypress', function(event) {
         if (event.which == 32)
             return false;
     });
